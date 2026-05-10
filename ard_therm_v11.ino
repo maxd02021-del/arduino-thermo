8807433dcf7ef5d8df8c047b1caad50983dc919a
maxd02021-del <maxd02021@gmail.com>
2026-05-10 02:26:21 +0100

    feat: introduced decide_power_mode() function
    Moved if statements for mode into decide_power_mode(). Also changed values for delayTime for each mode.

---
+++ ard_therm_v11.ino
@@ -0,0 +1,101 @@
+#include <math.h>
+const int B = 4275000; // B value of the thermistor
+const int R0 = 100000; // R0 = 100k
+const int pinTempSensor = A0; // Grove - Temperature Sensor connect to A0
+
+struct tempInfo {
+  float T0;
+  float T1;
+  float deltaT;
+} tempData;
+
+
+float delayTime = 100;
+int N = 0;
+float sample[32];
+float f_k = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  int a = analogRead(pinTempSensor);
+  float R = 1023.0/a-1.0;
+  R = R0*R;
+  tempData.T1 = tempData.T0 = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+}
+
+
+void apply_dft()
+{
+  float f_s = 1000 / delayTime;
+  
+
+  float real[32] = {0};
+  float imag[32] = {0};
+  float magnitude[32] = {0};
+
+  float maxMag = 0;
+  int dominantK = 0;
+
+  for (int k = 0; k < 32; ++k){
+    for (int n = 0; n < 32; ++n){
+      real[k] += sample[n] * cos(2 * PI * k * n / 32);
+      imag[k] += -sample[n] * sin(2 * PI * k * n / 32);
+    }
+  }
+
+  for (int k = 0; k < 32; ++k){
+    if (k == 0) continue;
+    magnitude[k] = sqrt(real[k] * real[k] + imag[k] * imag[k]);
+    if (magnitude[k] > maxMag){
+      maxMag = magnitude[k];
+      dominantK = k;
+    }
+  }
+  f_k = (dominantK * f_s) / 32;
+}
+
+ // Power mode and delay control
+void decide_power_mode(){
+  if (f_k >= 0.5){
+    delayTime = 500;
+    Serial.print("ACTIVE MODE");
+  } else if (f_k >= 0.1 && f_k < 0.5){
+    delayTime = 2000;
+    Serial.print("IDLE MODE");
+  } else if (f_k < 0.1){
+    delayTime = 10000;
+    Serial.print("LOW POWER MODE");
+  }
+}
+void loop()
+{
+  tempData.T0 = tempData.T1;
+  delay((unsigned long)delayTime);
+
+  int a = analogRead(pinTempSensor);
+  float R = 1023.0/a-1.0;
+  R = R0*R;
+  tempData.T1 = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+  sample[N] = tempData.T1;
+  ++N;
+
+  tempData.deltaT = tempData.T1 - tempData.T0;
+
+  if (N == 32) {
+    apply_dft();
+    N = 0;
+  }
+
+  decide_power_mode();
+ 
+  Serial.print(", Temperature = ");
+  Serial.print(tempData.T1);
+  Serial.print(", Change = ");
+  Serial.print(tempData.deltaT);
+  Serial.print(", DFT f_k = ");
+  Serial.println(f_k);
+
+}
